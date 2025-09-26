e3ee9fd1d27cdefc5f68b91101506dd06837a4b9
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-09-26 07:43:32 -0300

    feat(componnet-Login): Cria componente Login que agrega Titulo e Subtitulo e atualiza ReactDOM para renderizá-lo

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,5 +7,19 @@ function Titulo() {
 function SubTitulo() {
   return <h2 className="form__texto">Boas Vindas! Faça seu Login.</h2>;
 }
+function Login() {
+  return (
+    <div className="container-login">
+      <img
+        src="../img/imagem-login.png"
+        alt="uma mulher negra de cabelos crespos usando óculos e mexendo no computador, também há o logo da codeconnect"
+      />
+      <section>
+        <Titulo />
+        <SubTitulo />
+      </section>
+    </div>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Titulo />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Login />);
