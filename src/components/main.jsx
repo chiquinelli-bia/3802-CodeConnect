ecfb2acb14c34c90843dc549cadc6e39704d96f3
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-09-26 07:33:31 -0300

    feat(component-titulo): cria componente titulo e renderiza

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,11 @@
-function titulo() {
+import React from "react";
+import ReactDOM from "react-dom/client";
+function Titulo() {
   return <h1 className="form__titulo">Login</h1>;
 }
-function subTitulo() {
+
+function SubTitulo() {
   return <h2 className="form__texto">Boas Vindas! Faça seu Login.</h2>;
 }
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Titulo />);
