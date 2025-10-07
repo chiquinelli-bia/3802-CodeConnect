7f292515863b45e7702cf48de081a249316b4bdc
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-10-07 19:50:21 -0300

    feat(component-cadastro): cria componente cadastro. -importa componentes titulo e subtitulo e implementa -cria div container com imagem

--- src/components/cadastro.jsx
+++ src/components/cadastro.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Titulo, SubTitulo } from "./shared.jsx";
+import imagemCadastro from "../img/imagem-cadastro.png";
+
+function Cadastro() {
+  return (
+    <div className="container-login">
+      <img
+        src={imagemCadastro}
+        alt="Uma mulher de óculos trabalha em um laptop cercada por telas digitais verdes flutuantes que mostram pessoas. O ambiente é futurista e focado em tecnologia."
+      />
+      <section>
+        <form>
+          <Titulo>Cadastro</Titulo>
+          <SubTitulo>Olá! Preencha Seus Dados.</SubTitulo>
+        </form>
+      </section>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("rootCadastro")).render(
-  <Function />
+  <Cadastro />
 );
