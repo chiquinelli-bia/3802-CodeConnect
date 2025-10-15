d0a9e6aba7541224009bdb20091d1ee2f3bb8d1e
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-10-15 18:52:19 -0300

    refactor(publicar.js): adiciona link-destaque da navbar ao array botoesPublicar e adapta setupPublicar

--- src/modules/publicar.js
+++ src/modules/publicar.js
@@ -1,32 +1,46 @@
-const botaoPublicar = document.querySelector(".botao-publicar");
+const botoesPublicar = document.querySelectorAll(
+  ".botao-publicar, .link-destaque"
+);
+
 const listaTags = document.querySelector(".lista-tags");
 async function publicarProjeto(nomeDoProjeto, descricaoProjeto, tagsProjeto) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const deuCerto = Math.random() > 0.5;
-            if (deuCerto) {
-                resolve("Projeto publicado com sucesso.")
-            } else {
-                reject("Erro ao publicar o projeto.")
-            }
-        }, 2000)
-    })
-    
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const deuCerto = Math.random() > 0.5;
+      if (deuCerto) {
+        resolve(
+          "Simulação de envio — resultado aleatório, ainda em fase de testes.  Projeto publicado com sucesso."
+        );
+      } else {
+        reject(
+          "Simulação de envio — resultado aleatório, ainda em fase de testes. Erro ao publicar o projeto."
+        );
+      }
+    }, 2000);
+  });
 }
-export function setupPublicar () {
-    botaoPublicar.addEventListener("click", async (evento) => {
-        evento.preventDefault();
+export function setupPublicar() {
+  botoesPublicar.forEach((botao) => {
+    botao.addEventListener("click", async (evento) => {
+      evento.preventDefault();
 
-        const nomeDoProjeto = document.getElementById("nome").value;
-        const descricaoDoProjeto = document.getElementById("descricao").value;
-        const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
-        try {
-            const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
-            console.log(resultado);
-            alert("Deu tudo certo!")
-        } catch (error) {
-            console.log("Deu errado: ", error)
-            alert("Deu tudo errado!");
-        }
-    })
+      const nomeDoProjeto = document.getElementById("nome").value;
+      const descricaoDoProjeto = document.getElementById("descricao").value;
+      const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map(
+        (tag) => tag.textContent
+      );
+      try {
+        const resultado = await publicarProjeto(
+          nomeDoProjeto,
+          descricaoDoProjeto,
+          tagsProjeto
+        );
+        console.log(resultado);
+        alert(resultado);
+      } catch (error) {
+        console.log("Deu errado: ", error);
+        alert(error);
+      }
+    });
+  });
 }
