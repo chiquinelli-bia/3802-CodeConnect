2ac7edac043e37566f706b261748b473472a6cd2
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-10-20 07:06:13 -0300

    feat(btn-nav) habilita botão da nav

--- src/img/index.js
+++ src/img/index.js
@@ -3,5 +3,19 @@ import imagemCadastro from "./imagem-cadastro.png";
 import githubIcon from "./Github.svg";
 import googleIcon from "./Google.svg";
 import logo from "./logo.svg";
+import iconeLogin from "./login.png";
+import iconeFeed from "./feed.svg";
+import iconePerfil from "./account_circle.svg";
+import iconeSobre from "./info.svg";
 
-export { imagemLogin, imagemCadastro, githubIcon, googleIcon, logo };
+export {
+  imagemLogin,
+  imagemCadastro,
+  githubIcon,
+  googleIcon,
+  logo,
+  iconeLogin,
+  iconeFeed,
+  iconePerfil,
+  iconeSobre,
+};
