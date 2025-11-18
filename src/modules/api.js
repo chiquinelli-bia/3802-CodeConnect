6dc63f5ec1fc70d9d36254c493bf440e81acdc01
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-11-18 07:36:16 -0300

    feat(api): criar api.js com funções para buscar e filtrar projetos
    - adiciona buscarProjetos() com axios
    - adiciona filtrarProjetos() com filtro por título e por tags
    - define URL_BASE da API do Render
    - trata erros de requisição

---
+++ src/modules/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+const URL_BASE = "https://codeconnect-api-1.onrender.com/projetos";
+
+export async function buscarProjetos() {
+  try {
+    const cards = await axios.get(URL_BASE);
+    return cards.data;
+  } catch (error) {
+    alert("Erro ao buscar projetos.");
+    throw new Error("Erro ao buscar projetos");
+  }
+}
+
+export function filtrarProjetos(projetos, termoPesquisa, tagsSelecionadas) {
+  const termoLower = termoPesquisa.toLowerCase();
+
+  return projetos.filter((projeto) => {
+    const tituloValido = projeto.titulo.toLowerCase().includes(termoLower);
+    const tagsValido =
+      tagsSelecionadas.length === 0
+        ? true
+        : tagsSelecionadas.every((tag) =>
+            projeto.tags
+              .map((tag) => tag.toLowerCase())
+              .includes(tag.toLowerCase())
+          );
+
+    return tituloValido || tagsValido;
+  });
+}
