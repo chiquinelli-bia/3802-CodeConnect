b0c3f9065ef97e3266a3a407f4fd8b7d2d24f052
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-11-19 18:01:41 -0300

    refactor(PublicarProjeto): reorganiza setupPublicar para futura integração com API

--- src/modules/publicar.js
+++ src/modules/publicar.js
@@ -3,10 +3,34 @@ const botoesPublicar = document.querySelectorAll(
 );
 
 const listaTags = document.querySelector(".lista-tags");
-async function publicarProjeto(nomeDoProjeto, descricaoProjeto, tagsProjeto) {
+async function publicarProjeto(
+  id,
+  imagem_capa,
+  titulo,
+  resumo,
+  tags,
+  linhas_de_codigo,
+  compartilhamentos,
+  comentarios,
+  usuario,
+  conteudo_codigo
+) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const deuCerto = Math.random() > 0.5;
+      // mandar pra api
+      const deuCerto = console.log(
+        id,
+        imagem_capa,
+        titulo,
+        resumo,
+        tags,
+        linhas_de_codigo,
+        compartilhamentos,
+        comentarios,
+        usuario,
+        conteudo_codigo
+      );
+      // ajustar os alerts
       if (deuCerto) {
         resolve(
           "Simulação de envio — resultado aleatório, ainda em fase de testes.  Projeto publicado com sucesso."
@@ -23,17 +47,37 @@ export function setupPublicar() {
   botoesPublicar.forEach((botao) => {
     botao.addEventListener("click", async (evento) => {
       evento.preventDefault();
+      // buscar a foto
+      const id = 2;
+      const imagem_capa =
+        "https://github.com/MonicaHillman/codeconnect-api/blob/main/img/artigo2.png?raw=true";
 
-      const nomeDoProjeto = document.getElementById("nome").value;
-      const descricaoDoProjeto = document.getElementById("descricao").value;
-      const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map(
+      const titulo = document.getElementById("nome").value;
+      const resumo = document.getElementById("descricao").value;
+      const tags = Array.from(listaTags.querySelectorAll("p")).map(
         (tag) => tag.textContent
       );
+      const linhas_de_codigo = "x";
+      const compartilhamentos = "x";
+      const comentarios = "x";
+      const usuario = {
+        imagem:
+          "https://github.com/MonicaHillman/codeconnect-api/blob/main/img/icone2.png?raw=true",
+        nome: "Amanda",
+      };
+      const conteudo_codigo = "";
       try {
         const resultado = await publicarProjeto(
-          nomeDoProjeto,
-          descricaoDoProjeto,
-          tagsProjeto
+          id,
+          imagem_capa,
+          titulo,
+          resumo,
+          tags,
+          linhas_de_codigo,
+          compartilhamentos,
+          comentarios,
+          usuario,
+          conteudo_codigo
         );
         console.log(resultado);
         alert(resultado);
--- src/modules/scripts.js
+++ src/modules/scripts.js
@@ -1,15 +1,10 @@
 import { setupUpload } from "./upload.js";
-import { botaoLimpartags, setupTags } from "./tagsLogic.js";
 import { setupPublicar } from "./publicar.js";
 import { setupDescartar } from "./descartar.js";
 import { setupResponsividade } from "./responsividade.js";
-import { filtrarCards } from "./api.js";
 document.addEventListener("DOMContentLoaded", () => {
   setupUpload();
-  setupTags();
-  botaoLimpartags();
   setupPublicar();
   setupDescartar();
   setupResponsividade();
-  filtrarCards();
 });
