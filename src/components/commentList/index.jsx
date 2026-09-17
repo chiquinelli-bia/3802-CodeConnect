53ee343d0dbd6f1e6670a3b13777154db076be46
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-09-17 09:07:08 -0300

    feat(CommentList): cria componente estilizado

---
+++ src/components/commentList/index.jsx
@@ -0,0 +1,17 @@
+import { Comment } from "../comment";
+import styles from "./commentList.module.css";
+
+export const CommentList = ({ comments }) => {
+  return (
+    <section className={styles.comments}>
+      <h2>Comentários</h2>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>
+            <Comment comment={comment} key={comment.id} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
