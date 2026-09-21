432ff50f68ab0df845da4f292e438319250a25ec
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-09-21 07:47:20 -0300

    refactor(createProject): implementa formatacão do slug e adiciona ao envio do novo projeto

--- src/domain/useCases/createProject.ts
+++ src/domain/useCases/createProject.ts
@@ -2,6 +2,17 @@ import type { IProjectRepository } from "../repositories/IProjectRepository.ts";
 import type { ICreateProjectInput } from "../entities/IProject.ts";
 import { PROJECTS_COLLECTION } from "./api/config";
 
+function slugify(text: string): string {
+  return text
+    .toLowerCase()
+    .trim()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+}
+
 export class CreateProject {
   constructor(private projectRepository: IProjectRepository) {}
 
@@ -10,6 +21,13 @@ export class CreateProject {
       throw new Error("O título do projeto é obrigatório.");
     }
 
-    await this.projectRepository.create(projectData, PROJECTS_COLLECTION);
+    const slug = slugify(projectData.titulo);
+
+    const newProjectData = {
+      ...projectData,
+      slug,
+    };
+
+    await this.projectRepository.create(newProjectData, PROJECTS_COLLECTION);
   }
 }
